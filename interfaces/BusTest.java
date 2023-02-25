3ed5ed464b906529cb6640ce922c68e28ffee225
Muganga_Charles <104686307+muganga-charles@users.noreply.github.com>
2023-02-25 22:12:51 +0300

    Update BusTest.java

--- interfaces/BusTest.java
+++ interfaces/BusTest.java
@@ -1,6 +1,6 @@
 package interfaces;
 import java.util.*;
-public class BusTest {//ggh
+public class BusTest {//gg
 
 	public static void main(String[] args) {
 		Hybrid hybrid1 = new Hybrid(45, 1.2, 600, 150, 1);
